374caeece0efc7c7d0e7c2bf77c78a4be9c84fa8
tofy <54482950+t0fy@users.noreply.github.com>
2023-04-05 12:51:27 +0300

    Update 0xelkot.sh

--- 0xelkot.sh
+++ 0xelkot.sh
@@ -15,9 +15,13 @@ cat /root/$1/subs.txt | httpx -silent | anew /root/$1/alive.txt
 
 ## Test by Xray 
 
-for i in $(cat /root/$1/alive.txt); do xray_linux_amd64 ws --basic-crawler $i --plugins xss,sqldet,xxe,ssrf,cmd-injection,path-traversal --ho $(date +"%T").html ; done 
+for i in $(cat /root/$1/alive.txt); do ./xray_linux_amd64 ws --basic-crawler $i --plugins xss,sqldet,xxe,ssrf,cmd-injection,path-traversal --ho $(date +"%T").html ; done 
   
 
 ## test for nuclei 
 
-cat /root/$1/alive.txt | nuclei -t /root/cent-nuclei-templates -es info,unknown -etags ssl,network | anew /root/$1/nuclei.txt
+cat /root/$1/alive.txt | nuclei -t /root/cent/cent-nuclei-templates -es info,unknown -etags ssl,network | anew /root/$1/nuclei.txt
+
+## scan ffuf
+
+ffuf -c -w /root/$1/alive.txt:FUZZ1 -w /root/mywordlist/common.txt:FUZZ2 -u FUZZ1/FUZZ2 -mc 200 -ac -v -of html -o /root/$1/ffuf.html
